70d70c2459da116486ac57b445347b98e5f7a533
Yehhmii <franciscoabhulimen@gmail.com>
2025-09-25 12:37:28 -0600

    cleanedup unwanted components, updated the slash, app logo and login ui

--- lib/core/data/models/dahsboard_model.dart
+++ lib/core/data/models/dahsboard_model.dart
@@ -1,19 +1,15 @@
 class DashboardModel {
   final String lastLoggedIn;
-  final int totalVisitors;
-  final int totalApartmentVisited;
+
 
   DashboardModel({
     required this.lastLoggedIn,
-    required this.totalVisitors,
-    required this.totalApartmentVisited,
+
   });
 
   factory DashboardModel.fromJson(Map<String, dynamic> json) {
     return DashboardModel(
       lastLoggedIn: json['lastLoggedIn'] ?? "",
-      totalVisitors: json['totalVisitors'] ?? 0,
-      totalApartmentVisited: json['totalApartmentVisited'] ?? 0,
     );
   }
 }
